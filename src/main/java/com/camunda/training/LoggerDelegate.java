4f2d3a5a04a876b9110f519a7c2921124dbef06d
norman.luering@tu-dortmund.de <norman.luering@camunda.com>
2021-07-13 13:56:15 +0200

    Finish Exercise 5

---
+++ src/main/java/com/camunda/training/LoggerDelegate.java
@@ -0,0 +1,24 @@
+package com.camunda.training;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerDelegate implements JavaDelegate {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(LoggerDelegate.class.getName());
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+
+        LOGGER.info("\n\n  ... LoggerDelegate invoked by "
+                + "processDefinitionId=" + execution.getProcessDefinitionId()
+                + ", activtyId=" + execution.getCurrentActivityId()
+                + ", activtyName='" + execution.getCurrentActivityName() + "'"
+                + ", processInstanceId=" + execution.getProcessInstanceId()
+                + ", businessKey=" + execution.getProcessBusinessKey()
+                + ", executionId=" + execution.getId()
+                + " \n\n");
+    }
+}
--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,25 +1,33 @@
 package com.camunda.training;
 
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.mock.Mocks;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
-import static org.assertj.core.api.Assertions.*;
 
 public class ProcessJUnitTest {
 
   @Rule
-  public ProcessEngineRule rule = new ProcessEngineRule();
+  @ClassRule
+  public static ProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().build();
 
   @Before
   public void setup() {
+    Mocks.register("createTweetDelegate", new LoggerDelegate());
     init(rule.getProcessEngine());
   }
 
@@ -28,11 +36,24 @@ public class ProcessJUnitTest {
   public void testHappyPath() {
     // Create a HashMap to put in variables for the process instance
     Map<String, Object> variables = new HashMap<String, Object>();
-    variables.put("approved", true);
+    variables.put("content", "Running from JUnit Test with Random Number: " + ThreadLocalRandom.current().nextInt());
     // Start process with Java API and variables
     ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
+    assertThat(processInstance).isStarted();
     // Make assertions on the process instance
-    assertThat(processInstance).isEnded();
+    /* Map<String, Object> userTaskVariables = new HashMap<String, Object>();
+    userTaskVariables.put("approved", true);
+    List<Task> taskList = taskService().createTaskQuery().taskCandidateGroup("management").processInstanceId(processInstance.getId()).list();
+    org.assertj.core.api.Assertions.assertThat(taskList).isNotNull();
+    org.assertj.core.api.Assertions.assertThat(taskList).hasSize(1);
+    Task task = taskList.get(0);
+    taskService().complete(task.getId(), userTaskVariables); */
+
+    //Fast Way to complete UserTask with mandatory checks
+    assertThat(processInstance).isWaitingAt("ReviewTweet_UserTask");
+    complete(task("ReviewTweet_UserTask"), withVariables("approved", true));
+
+    assertThat(processInstance).isEnded().hasPassed("PublishTweet_ServiceTask");
   }
 
 }
