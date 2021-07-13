2930ebe83e14557e4b2f19b6bc4e007bbb3d1435
norman.luering@tu-dortmund.de <norman.luering@camunda.com>
2021-07-13 09:33:10 +0200

    Exercise 3a

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,10 +1,38 @@
 package com.camunda.training;
 
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.test.Deployment;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.assertj.core.api.Assertions.*;
+
 public class ProcessJUnitTest {
-  
-//  @Test
+
+  @Rule
+  public ProcessEngineRule rule = new ProcessEngineRule();
+
+  @Before
+  public void setup() {
+    init(rule.getProcessEngine());
+  }
+
+  @Test
+  @Deployment(resources = "twitterqa.bpmn")
   public void testHappyPath() {
-    
+    // Create a HashMap to put in variables for the process instance
+    Map<String, Object> variables = new HashMap<String, Object>();
+    variables.put("approved", true);
+    // Start process with Java API and variables
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
+    // Make assertions on the process instance
+    assertThat(processInstance).isEnded();
   }
 
 }
